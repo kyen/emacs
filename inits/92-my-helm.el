249479144f16a770acad00d97afc0aebb641b45d
Kenya Yabe <kenya.yabe@gmail.com>
2015-08-22 15:06:57 +0900

    modify helm-migemo for swoop

--- inits/92-my-helm.el
+++ inits/92-my-helm.el
@@ -9,6 +9,20 @@
 (require 'helm-flymake)
 (require 'helm-migemo)
 
+;;; Ç±ÇÃèCê≥Ç™ïKóv(migemo and swoop)
+(eval-after-load "helm-migemo"
+  '(defun helm-compile-source--candidates-in-buffer (source)
+     (helm-aif (assoc 'candidates-in-buffer source)
+         (append source
+                 `((candidates
+                    . ,(or (cdr it)
+                           (lambda ()
+                             ;; Do not use `source' because other plugins
+                             ;; (such as helm-migemo) may change it
+                             (helm-candidates-in-buffer (helm-get-current-source)))))
+                   (volatile) (match identity)))
+       source)))
+
 ;; customize
 (progn
   (require 'helm-ls-git)
